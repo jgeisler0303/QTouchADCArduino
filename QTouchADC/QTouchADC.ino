6a11afd9afcfe29baa40cc10a3a32102f0f3f063
Jens Geisler <jens.geisler@web.de>
2013-04-13 22:21:05 +0200

    comments to explain every line of the code

--- QTouchADC/QTouchADC.ino
+++ QTouchADC/QTouchADC.ino
@@ -1,104 +1,111 @@
-int adc1,adc0;
-unsigned int adc2, adc3;
+unsigned int adc0; // temp var
+unsigned int adc1, adc2; // store the avarage of the charge resp. discharge measurement
+unsigned int probe_val; // store the resulting touch measurement
 
-byte pwm;
+// ADC constants
+#define ADMUX_MASK  0b00001111 // mask the mux bits in the ADMUX register
+#define MUX_GND 0b00001111 // mux value for connecting the ADC unit internally to GND
+#define MUX_1p1V 0b0001110 // mux value for connecting the ADC unit to the internal 1.1V band gap reference
+#define MUX_REF_VCC 0b01000000 // value to set the ADC reference to Vcc
+#define MUX_REF_1p1 0b11000000 // value to set the ADC reference to the internal 1.1V band gap reference
 
-#define ADMUX_MASK  0b00001111
-#define MUX_GND 0b00001111
-#define MUX_1p1V 0b0001110
-#define MUX_REF_VCC 0b01000000
-#define MUX_REF_1p1 0b11000000
-
-const uint16_t pwmtable_8D[32] =
-{0, 1, 1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 9, 10, 12, 15, 17, 21, 25, 30, 36, 43, 51, 61, 73, 87, 104, 125, 149, 178, 213, 255
-};
+const uint16_t ledFadeTable[32] = {0, 1, 1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 9, 10, 12, 15, 17, 21, 25, 30, 36, 43, 51, 61, 73, 87, 104, 125, 149, 178, 213, 255}; // this is an exponential series to model the perception of the LED brightness by the human eye
 
 
-#define QTDELAY  5 //Wartezeit in mikrosekunden
+#define QTDELAY  5 // time it takes for the capacitors to get charged/discharged in microseconds
+#define TOUCH_VALUE_BASELINE 333 // this is the value my setup measures when the probe is not touched. For your setup this might be different. In order for the LED to fade correctly, you will have to adjust this value
+#define TOUCH_VALUE_SCALE 5 // this is also used for the LED fading. The value should be chosen such that the value measured when the probe is fully touched minus TOUCH_VALUE_BASELINE is scaled to 31, e.g. untouched_val= 333, touched_val= 488, difference= 155, divide by 5 to get 31.
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // standard serial setup
  
-  ADMUX = 0b01000000; // Vcc as voltage reference (76), right adjustment (5), use ADC0 as input (3210)
-  ADCSRA = 0b11000111; // enable ADC (7), initialize ADC (6), no autotrigger (5), don't clear int-flag ADIF (4), no interrupt ADIE (3), clock div by 128@16Mhz=125kHz ADPS (210)
-  ADCSRB = 0b00000000; // no analog compare ACME (6), autotrigger scource free running ADTS, (210)
+  // prepare the ADC unit for one-shot measurements
+  // see the atmega328 datasheet for explanations of the registers and values
+  ADMUX = 0b01000000; // Vcc as voltage reference (bits76), right adjustment (bit5), use ADC0 as input (bits3210)
+  ADCSRA = 0b11000111; // enable ADC (bit7), initialize ADC (bit6), no autotrigger (bit5), don't clear int-flag  (bit4), no interrupt (bit3), clock div by 128@16Mhz=125kHz (bit210) ADC should run at 50kHz to 200kHz
+  ADCSRB = 0b00000000; // autotrigger source free running (bits210) doesn't apply
   while(ADCSRA & (1<<ADSC)){  } // wait for first conversion to complete
-  adc1 = ADC;
-  //pinMode(13, OUTPUT);
-  // DDRC = 0x00;                //Port auf Ausgang setzten
+  adc0 = ADC; // not sure if the value of the conversion has to be read if not needed
 }
  
  
 void loop() {
+  // 16 measurements are taken an d averaged to improve noise immunity
   for (int i=0; i<16; i++) {
+    // first measurement: charge touch probe, discharge ADC s&h cap, connect the two, measure the volage
     // charge sensor
-    DDRC = 0b00000001;                //Port auf Ausgang setzten
-    PORTC = 0b00000001;          // pullup PINC0 auf High um CS zu laden und PINC1 auf LOW um C Sample & Hold entladen zu können
+    DDRC= 0b00000001; // config pin as push-pull output (using the internal pullup in input mode is too slow). Beware this way of setting the port config changes the whole of PORTC!! DDRC|= 0b00000001 would be better
+    PORTC= 0b00000001; // set PINC0 high to charge the touch probe cap
     
-    // discharge
-    ADMUX = MUX_REF_VCC | MUX_GND; //select ADC1 to discharge s&h cap
-    ADCSRA |= (1<<ADSC);          // start conversion
-    while(ADCSRA & (1<<ADSC)){  } // wait for conversion complete
-    adc0 = ADC;                  // save conversion
-    delayMicroseconds(QTDELAY);        // kurz warten bis geladen
+    // discharge s&h cap by measureing the internal GND reference
+    ADMUX = MUX_REF_VCC | MUX_GND; // select the internal GND reference as input to the ADC unit
+    ADCSRA |= (1<<ADSC); // apparently selecting the input is not enough. ASo, a conversion has to be started
+    while(ADCSRA & (1<<ADSC)){  } // wait for conversion to complete
+    adc0= ADC; // save conversion to dummy var. maybe not necessary?
     
-    DDRC = 0x00;                //Port auf Ausgang setzten
-    PORTC = 0x00;                  //Tristate
+    delayMicroseconds(QTDELAY); // wait for the touch probe cap to be fully charged
     
-    // charge s&h cap
-    ADMUX = MUX_REF_VCC;  // select ADC0
-    delayMicroseconds(QTDELAY);          //warten?! geht auch ohne...
+    DDRC= 0x00; // config pin as input. Beware this way of setting the port config changes the whole of PORTC!! DDRC&= ~0b00000001 would be better
+    PORTC= 0x00; // disable the internal pullup to make the port tristate
+    
+    // connect touch probe cap to s&h cap to transfer the charge
+    ADMUX= MUX_REF_VCC; // select ADC0 pin as ADC input (PINC0), setting Vcc as ADC reference voltage
+    
+    delayMicroseconds(QTDELAY); // wait for charge to be transfered. maybe not necessary?
     
     // measure
-    ADCSRA |= (1<<ADSC);            //Messung
-    while(ADCSRA & (1<<ADSC)){  }
-    adc1 = ADC;
-    adc2 += adc1;                  //Mittelwertbildung
+    ADCSRA|= (1<<ADSC); // start measurement
+    while(ADCSRA & (1<<ADSC)){  } // wait for conversion to complete
+    adc0= ADC; // save conversion result
+    adc1+= adc0; // accumulate the results for the averaging
 
+    // second measurement:discharge touch probe, charge ADC s&h cap, connect the two, measure the volage
     // discharge sensor
-    DDRC = 0b00000001;                //Port auf Ausgang setzten
-    PORTC = 0b00000000;          // pullup PINC0 auf High um CS zu laden und PINC1 auf LOW um C Sample & Hold entladen zu können
+    DDRC= 0b00000001; // config pin as push-pull output
+    PORTC= 0b00000000; // set PINC0 low to discharge the touch probe cap
     
-    // charge
-    ADMUX = MUX_REF_1p1 | MUX_1p1V; //select ADC1 to discharge s&h cap
-    ADCSRA |= (1<<ADSC);          // start conversion
-    while(ADCSRA & (1<<ADSC)){  } // wait for conversion complete
-    adc0 = ADC;                  // save conversion
-    delayMicroseconds(QTDELAY);        // kurz warten bis geladen
+    // charge  s&h cap by measuring the internal 1.1V reference
+    ADMUX= MUX_REF_1p1 | MUX_1p1V; // select the internal 1.1V reference as input to the ADC unit and also as reference for the ADC conversion
+    ADCSRA|= (1<<ADSC); // apparently selecting the input is not enough. So, a conversion has to be started
+    while(ADCSRA & (1<<ADSC)){  }// wait for conversion to complete
+    adc0= ADC; // save conversion to dummy var. maybe not necessary?
     
-    DDRC = 0x00;                //Port auf Ausgang setzten
-    //PORTC = 0x00;                  //Tristate
+    delayMicroseconds(QTDELAY); // wait for the touch probe cap to be fully discharged
     
-    // charge s&h cap
-    ADMUX = MUX_REF_1p1;  // select ADC0
-    delayMicroseconds(QTDELAY);          //warten?! geht auch ohne...
+    DDRC= 0x00; // config pin as input (pullup is already disabled)
+    
+    // connect touch probe cap to s&h cap to transfer the charge
+    ADMUX = MUX_REF_1p1; // select ADC0 pin as ADC input (PINC0), setting internal 1.1V as ADC reference voltage
+    delayMicroseconds(QTDELAY); // wait for charge to be transfered. maybe not necessary?
     
     // measure
-    ADCSRA |= (1<<ADSC);            //Messung
-    while(ADCSRA & (1<<ADSC)){  }
-    adc1 = ADC;
-    adc3 += adc1;                  //Mittelwertbildung
+    ADCSRA |= (1<<ADSC); // start measurement
+    while(ADCSRA & (1<<ADSC)){  } // wait for conversion to complete
+    adc0= ADC; // save conversion result
+    adc2+= adc0;// accumulate the results for the averaging
   }
+  adc1>>=4; // divide the accumulated measurements by 16
   adc2>>=4;
-  adc3>>=4;
  
-  int16_t idx= (adc2-adc3-333);
-  if(idx<0) idx= 0;
-  idx/= 5;
-  if(idx>31) idx= 31;
+  // resulting raw touch value
+  probe_val= adc1-adc2; // the value of adc1 (probe charged) gets higher when the probe ist touched, the value of adc2 (s&h charged) gets lower when the probe ist touched, so, it has to be be subtracted to amplify the detection accuracy
+  
+  // calculate the index to the LED fading table
+  int16_t idx= (probe_val-TOUCH_VALUE_BASELINE); // offset probe_val by value of untouched probe
+  if(idx<0) idx= 0; // limit the index!!!
+  idx/= TOUCH_VALUE_SCALE; // scale the index
+  if(idx>31) idx= 31; // limit the index!!!
 
-  Serial.print(adc2);
+  // print some info to the serial
+  Serial.print(probe_val);
   Serial.print('|');
-  Serial.print(idx);
-  Serial.print('|');
-  Serial.print(adc2-adc3);
-  Serial.print('|');
-  analogWrite(9, pwmtable_8D[idx]);
-  Serial.println(pwmtable_8D[idx]);
-  pwm++;
-  if(pwm>32) pwm= 0;
-  adc2=0;
-  adc3= 0;
-  delay(10);                          //je nachdem wieviele Messungen man pro sekunde haben möchte...
+  Serial.println(idx);
+  
+  // fade the LED
+  analogWrite(9, ledFadeTable[idx]);
+  
+  adc1= 0; // clear the averaging variables for the next run
+  adc2= 0;
+  delay(10); // take 100 measurements per second
 }
  
